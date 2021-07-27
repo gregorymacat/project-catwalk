f5e0094b25b8b0baebbd81991a21c22701a40e08
Gregory Macat <gregorymacat@gmail.com>
2021-07-27 16:06:19 -0400

    Arrows working for card carousel component

--- client/src/Components/Related-Outfit/CardCarousel.jsx
+++ client/src/Components/Related-Outfit/CardCarousel.jsx
@@ -10,27 +10,52 @@ var CardCarousel = function(props){
     <React.Fragment>
       {/* <Arrow direction='left'/> */}
       <div className='arrow-left'>
-        <img src='Assets/Icons/ArrowBack/2x/outline_arrow_back_black_24dp.png'></img>
+        <img src='Assets/Icons/ArrowBack/2x/outline_arrow_back_black_24dp.png'
+          onClick={props.handleClick} id="arrow-back"></img>
       </div>
       <div className='card-container'>
         {
-          props.products.map((product) => {
-            return (
-            <div key={product.id} className='product-card'>
-              <p>{product.category}</p>
-              <p>{product.name}</p>
-              <p>{product.default_price}</p>
-            </div>
-            )
-          })
+          chooseCards(props.startIndex, props.products)
+          // props.products.map((product) => {
+          //   return (
+          //   <div key={product.id} className='product-card'>
+          //     <p>{product.category}</p>
+          //     <p>{product.name}</p>
+          //     <p>{product.default_price}</p>
+          //   </div>
+          //   )
+          // })
         }
       </div>
       <div className='arrow-right'>
-        <img src='Assets/Icons/ArrowForward/2x/outline_arrow_forward_black_24dp.png'></img>
+        <img src='Assets/Icons/ArrowForward/2x/outline_arrow_forward_black_24dp.png'
+          onClick={props.handleClick} id="arrow-forward"></img>
       </div>
       {/* <Arrow direction='right'/> */}
     </React.Fragment>
   )
+}
+
+var chooseCards = function(index, products) {
+  var displayCards = [];
+  if (products.length === 0) {
+    return displayCards;
+  }
+  for (var i = index; i <= index + 3; i++) {
+    var currentIndex = i;
+    if (i >= products.length){
+      currentIndex = i - products.length;
+    }
+    var currentProduct = products[currentIndex];
+    displayCards.push(
+      <div key={currentProduct.id} className='product-card'>
+        <p>{currentProduct.category}</p>
+        <p>{currentProduct.name}</p>
+        <p>{currentProduct.default_price}</p>
+      </div>
+    );
+  }
+  return displayCards;
 }
 
 export default CardCarousel;
--- client/src/Components/Related-Outfit/RelatedOutfit.jsx
+++ client/src/Components/Related-Outfit/RelatedOutfit.jsx
@@ -4,13 +4,36 @@ import CardCarousel from './CardCarousel.jsx';
 class RelatedOutfit extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      displayIndex: 0
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(event) {
+    console.log(event.target.id);
+    var id = event.target.id;
+    if (id === 'arrow-back') {
+      if (this.state.displayIndex === 0) {
+        this.setState({displayIndex: this.props.products.length - 1});
+      } else {
+        this.setState({displayIndex: this.state.displayIndex - 1});
+      }
+    } else if (id === 'arrow-forward') {
+      if (this.state.displayIndex === this.props.products.length - 1) {
+        this.setState({displayIndex: 0});
+      } else {
+        this.setState({displayIndex: this.state.displayIndex + 1});
+      }
+    }
   }
 
   render(){
     console.log(this.props.products);
     return(
       <div className='related'>
-        <CardCarousel products={this.props.products}/>
+        <CardCarousel startIndex={this.state.displayIndex} products={this.props.products}
+          handleClick={this.handleClick}/>
       </div>
     )
   }
