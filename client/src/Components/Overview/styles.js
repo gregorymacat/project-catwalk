0d1810e79d2a3dd3f2687600d08e3726d90c743a
Melika Campbell <m3likas@gmail.com>
2021-07-30 14:04:56 -0700

    added layout and styled product information

---
+++ client/src/Components/Overview/styles.js
@@ -0,0 +1,46 @@
+export default {
+  overview: {
+    display: "flex",
+    backgroundColor: "#d3d3d3",
+    height: "100vh",
+    flexDirection: "column"
+  },
+  row: {
+    display: "flex",
+    flexDirection: "row"
+  },
+  carousel: {
+    backgroundColor: "grey",
+    display: "flex",
+    height: "400px",
+    width: "50%",
+    padding: "20px"
+  },
+  productInfo: {
+    padding: "20px",
+    width: "50%"
+  },
+  productStyle: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  colorCircles: {
+    display: "flex"
+  },
+  circle: {
+    margin: "10px",
+    borderRadius: "50%",
+    height: "50px",
+    width: "50px",
+    backgroundColor: "purple"
+  },
+  select: {
+    backgroundColor: "transparent",
+    height: "30px",
+    width: "auto",
+    margin: "10px"
+  },
+  productDetails: {
+    margin: "20px"
+  }
+};
