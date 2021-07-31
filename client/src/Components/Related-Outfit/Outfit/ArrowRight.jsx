ad7c61846ea04814db4ee64315795fc3d19ed0d4
Gregory Macat <gregorymacat@gmail.com>
2021-07-31 15:26:23 -0400

    Displaying outfit carousel, add card works, scrolling has dupe bug

---
+++ client/src/Components/Related-Outfit/Outfit/ArrowRight.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+var ArrowRight = function(props) {
+  console.log(props.index);
+  var handleClick = () => {
+    var index = props.index;
+    // console.log(index);
+    if (index + 1 < props.max - 1) {
+      props.click({
+        displayIndex: index + 1,
+        leftVisible: true,
+        rightVisible: true
+      });
+    } else {
+      props.click({
+        displayIndex: index + 1,
+        rightVisible: false
+      });
+    }
+  }
+
+  if (props.isDisplaying) {
+    return (
+        <div className='carousel right'>
+          <img src='Assets/Icons/ArrowForward/2x/outline_arrow_forward_black_24dp.png'
+          onClick={handleClick}></img>
+        </div>
+    )
+  }
+  return <div className='carousel right'></div>;
+}
+
+export default ArrowRight;
