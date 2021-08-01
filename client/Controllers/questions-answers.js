147b5cb1be8f0c19eff1b47fd36ac67586768985
Sumit Patel <sumitz1207@yandex.com>
2021-08-01 12:49:38 -0400

    question/answer list working, get request instead of sampledata, search working

--- client/Controllers/questions-answers.js
+++ client/Controllers/questions-answers.js
@@ -2,6 +2,18 @@ var axios = require('axios');
 
 module.exports = {
 //GET REQUESTS
+  getQuestions: function(req, callback) {
+    axios.get('/qa/questions/?page=1&count=5&sort=newest&product_id=' + req)
+      .then((response) => {
+        callback(null, response.data);
+      })
+      .catch((error) => {
+        console.log('error in getting questions');
+        throw error;
+        return callback(error);
+      });
+  },
+
   getAnswersByQuestionId: function(id, callback) {
     axios.get('/qa/questions/' + id + '/answers')
       .then((response) => {
@@ -17,6 +29,10 @@ module.exports = {
 //POST REQUEST
   postAnswersByQuestionId: function(id, callback) {
     axios.post('/qa/questions/' + id + '/answers')
+  },
+
+  postQuestion: function(id, callback) {
+    axios.post('/qa/questions')
   },
 
 //PUT REQUESTS
